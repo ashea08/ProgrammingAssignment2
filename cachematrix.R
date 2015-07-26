b6f7321a946f4d5edf93638a7af8a741d9b2c60f
A Colombo <asheac08@yahoo.com>
2015-07-26 14:11:31 -0700

    Updated R code for Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions below take a matrix as input and prep it to be solved
+## for its inverse and cached for future use. 
 
-## Write a short comment describing this function
+## The makeCacheMatrix takes a matrix as input and preps it to be used in the
+## cacheSolve function. The function sets the matrix and creates a getinverse
+## function that is used in cacheSolve to access the matrix. The function
+## returns a list.
 
 makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
+    set <- function(y) {      ## matrix passed into function is set here.
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x       ## matrix is assigned to get function here.     
+    setinverse <- function(inverse) m <<- inverse
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes the matrix assigned to "m" in makeCacheMatrix and solves
+## for its inverse or if the inverse already exists, it returns the inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()         ## Accessing just the getinverse subset of x 
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()             
+    m <- solve(data, ...)
+    x$setinverse(m)             ## Assigns inverse to "setinverse" subset
+    m                           ## Returns inverse
+                        
 }
